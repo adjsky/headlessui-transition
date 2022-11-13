3d807caf5f1c79bff3c88018d08328ae41fc4a4f
adjsky <igorlfmartins@mail.ru>
2022-11-13 23:12:13 +0300

    bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,26 @@
-/** Add your relevant code here for the issue to reproduce */
+import { Transition } from "@headlessui/react";
+
 export default function Home() {
-  return null
+  return (
+    <>
+      <Transition
+        show
+        appear
+        className="opacity-0"
+        enter="transition-opacity duration-500"
+        enterTo="!opacity-100"
+      >
+        will fade in
+      </Transition>
+      <Transition
+        show
+        appear
+        enter="transition-opacity duration-500"
+        enterTo="opacity-100"
+        enterFrom="opacity-0"
+      >
+        will not fade in
+      </Transition>
+    </>
+  );
 }
